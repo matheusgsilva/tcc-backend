7e0f0c27f44fb9abe56d1c96d9e5ca830b6919e0
Matheus Silva <matheus22gds@gmail.com>
2023-06-17 09:40:20 -0300

    ajustes

--- src/main/java/br/senac/backend/repository/PetRepository.java
+++ src/main/java/br/senac/backend/repository/PetRepository.java
@@ -31,6 +31,18 @@ public interface PetRepository extends PagingAndSortingRepository<Pet, Long> {
 
 	@Query("SELECT c from Pet c WHERE ((:description is null or c.description LIKE CONCAT('%',:description,'%')) "
 			+ "and (:size is null or c.size LIKE CONCAT('%',:size,'%')) "
+			+ "and (:typePet is null or c.typePet LIKE CONCAT('%',:typePet,'%')) "
+			+ "and (:gender is null or c.gender LIKE CONCAT('%',:gender,'%')) "
+			+ "and (:city is null or c.company.city LIKE CONCAT('%',:city,'%')) "
+			+ "and (:companyName is null or c.company.name LIKE CONCAT('%',:companyName,'%')) "
+			+ "and (:district is null or c.company.district LIKE CONCAT('%',:district,'%'))) "
+			+ "and c.active = 0 and c.company.active = 0")
+	List<Pet> getAllFilteredWithOutBreed(@Param("description") String description, @Param("size") String size,
+			@Param("typePet") String typePet, @Param("city") String city, @Param("district") String district,
+			@Param("gender") String gender, @Param("companyName") String companyName);
+
+	@Query("SELECT c from Pet c WHERE ((:description is null or c.description LIKE CONCAT('%',:description,'%')) "
+			+ "and (:size is null or c.size LIKE CONCAT('%',:size,'%')) "
 			+ "and (:breed is null or c.breed LIKE CONCAT('%',:breed,'%'))"
 			+ "and (:typePet is null or c.typePet LIKE CONCAT('%',:typePet,'%')) "
 			+ "and (:gender is null or c.gender LIKE CONCAT('%',:gender,'%')) "
@@ -39,8 +51,9 @@ public interface PetRepository extends PagingAndSortingRepository<Pet, Long> {
 			+ "and c.active = 0 and c.company.active = 0 and c.company.guid = :companyGuid")
 	List<Pet> getAllFilteredCompany(@Param("description") String description, @Param("size") String size,
 			@Param("breed") String breed, @Param("typePet") String typePet, @Param("city") String city,
-			@Param("district") String district, @Param("companyGuid") String companyGuid, @Param("gender") String gender);
-	
+			@Param("district") String district, @Param("companyGuid") String companyGuid,
+			@Param("gender") String gender);
+
 	@Query("SELECT CASE WHEN (COUNT(p) > 0) THEN true ELSE false END FROM Pet p WHERE "
 			+ "((:size is null or p.size LIKE CONCAT('%',:size,'%')) "
 			+ "and (:breed is null or p.breed LIKE CONCAT('%',:breed,'%')) "
